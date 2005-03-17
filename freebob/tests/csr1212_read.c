0aeb91dc6205f2c670203aeb11252912b6badcf8
wagi <wagi@2be59082-3212-0410-8809-b0798e1608f0>
2005-03-17 23:11:17 +0000

    'browsing through the config rom' added.
    git-svn-id: http://subversion.ffado.org/ffado/trunk@78 2be59082-3212-0410-8809-b0798e1608f0

--- freebob/tests/csr1212_read.c
+++ freebob/tests/csr1212_read.c
@@ -18,6 +18,10 @@
  * MA 02111-1307 USA.
  */
 
+/* The process_unit_directory and process_root_directory function are
+ * based on code from linux-2.6.11/drivers/ieee1394/nodemgr.c.
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -34,10 +38,12 @@ freebob_bus_read(struct csr1212_csr* csr, u_int64_t addr, u_int16_t length,
 		 void* buffer, void* private)
 {
     struct freebob_csr_info* csr_info = (struct freebob_csr_info*) private;
+    /*
     printf("freebob_bus_read: node id = %d, addr = 0x%08x%08x, length = %d\n", 
 	   csr_info->node_id, 
 	   (unsigned int)(addr >> 32), (unsigned int)(addr &0xffffffff), 
 	   length);
+    */
 
     unsigned int retval;
     retval=raw1394_read(csr_info->handle, csr_info->node_id, addr,
@@ -68,6 +74,117 @@ static struct csr1212_bus_ops freebob_csr_ops = {
     .bus_read = freebob_bus_read,
     .get_max_rom = freebob_get_max_rom
 };
+
+static void 
+process_unit_directory(struct csr1212_csr* csr, 
+		       struct csr1212_keyval *ud_kv,
+		       unsigned int *id)
+{
+    struct csr1212_dentry *dentry;
+    struct csr1212_keyval *kv;
+    unsigned int last_key_id = 0;
+
+    printf("process unit directory:\n");
+    csr1212_for_each_dir_entry(csr, kv, ud_kv, dentry) {
+	switch (kv->key.id) {
+	    case CSR1212_KV_ID_VENDOR:
+		if (kv->key.type == CSR1212_KV_TYPE_IMMEDIATE) {
+		    printf("\tvendor_id = 0x%08x\n", 
+			   kv->value.immediate);
+		}
+		break;
+
+	    case CSR1212_KV_ID_MODEL:
+		printf("\tmodel_id = 0x%08x\n", 
+		       kv->value.immediate);
+		break;
+
+	    case CSR1212_KV_ID_SPECIFIER_ID:
+		printf("\tspecifier_id = 0x%08x\n",
+		       kv->value.immediate);
+		break;
+
+	    case CSR1212_KV_ID_VERSION:
+		printf("\tversion = 0x%08x\n",
+		       kv->value.immediate);
+		break;
+
+	    case CSR1212_KV_ID_DESCRIPTOR:
+		if (kv->key.type == CSR1212_KV_TYPE_LEAF &&
+		    CSR1212_DESCRIPTOR_LEAF_TYPE(kv) == 0 &&
+		    CSR1212_DESCRIPTOR_LEAF_SPECIFIER_ID(kv) == 0 &&
+		    CSR1212_TEXTUAL_DESCRIPTOR_LEAF_WIDTH(kv) == 0 &&
+		    CSR1212_TEXTUAL_DESCRIPTOR_LEAF_CHAR_SET(kv) == 0 &&
+		    CSR1212_TEXTUAL_DESCRIPTOR_LEAF_LANGUAGE(kv) == 0) 
+		{
+		    switch (last_key_id) {
+			case CSR1212_KV_ID_VENDOR:
+			    csr1212_keep_keyval(kv);
+			    break;
+
+			case CSR1212_KV_ID_MODEL:
+			    csr1212_keep_keyval(kv);
+			    break;
+
+		    }
+		} /* else if (kv->key.type == CSR1212_KV_TYPE_DIRECTORY) ... */
+		break;
+
+	    case CSR1212_KV_ID_DEPENDENT_INFO:
+		if (kv->key.type == CSR1212_KV_TYPE_DIRECTORY) {
+		    /* This should really be done in SBP2 as this is
+		     * doing SBP2 specific parsing. */
+		    process_unit_directory(csr, kv, id);
+		}
+
+		break;
+
+	    default:
+		break;
+	}
+	last_key_id = kv->key.id;
+    }
+}
+
+static void 
+process_root_directory(struct csr1212_csr* csr)
+{
+    unsigned int ud_id = 0;
+    struct csr1212_dentry *dentry;
+    struct csr1212_keyval *kv;
+    unsigned int last_key_id = 0;
+
+    csr1212_for_each_dir_entry(csr, kv, csr->root_kv, dentry) {
+	switch (kv->key.id) {
+	    case CSR1212_KV_ID_VENDOR:
+		printf("vendor id = 0x%08x\n", kv->value.immediate);
+		break;
+
+	    case CSR1212_KV_ID_NODE_CAPABILITIES:
+		printf("capabilities = 0x%08x\n", kv->value.immediate);
+		break;
+
+	    case CSR1212_KV_ID_UNIT:
+		process_unit_directory(csr, kv, &ud_id);
+		break;
+
+	    case CSR1212_KV_ID_DESCRIPTOR:
+		if (last_key_id == CSR1212_KV_ID_VENDOR) {
+		    if (kv->key.type == CSR1212_KV_TYPE_LEAF &&
+			CSR1212_DESCRIPTOR_LEAF_TYPE(kv) == 0 &&
+			CSR1212_DESCRIPTOR_LEAF_SPECIFIER_ID(kv) == 0 &&
+			CSR1212_TEXTUAL_DESCRIPTOR_LEAF_WIDTH(kv) == 0 &&
+			CSR1212_TEXTUAL_DESCRIPTOR_LEAF_CHAR_SET(kv) == 0 &&
+			CSR1212_TEXTUAL_DESCRIPTOR_LEAF_LANGUAGE(kv) == 0) {
+			csr1212_keep_keyval(kv);
+		    }
+		}
+		break;
+	}
+	last_key_id = kv->key.id;
+    }
+}
+
 
 int
 main(int argc, char* argv[])
@@ -118,7 +235,7 @@ main(int argc, char* argv[])
 	return -1;
     }
 
-    // still no fancy stuff here...
+    process_root_directory(csr);
         
     csr1212_destroy_csr(csr);
 
