a983b2cb4347b6f485a772ec4f6f288116817602
wagi <wagi@2be59082-3212-0410-8809-b0798e1608f0>
2006-04-24 20:49:08 +0000

    CVS-SVN migration developer public cvs syncronization patch
    git-svn-id: http://subversion.ffado.org/ffado/trunk@184 2be59082-3212-0410-8809-b0798e1608f0

--- alsa-plugin/freebob/pcm_freebob.c
+++ alsa-plugin/freebob/pcm_freebob.c
@@ -35,10 +35,58 @@
 
 #include <pthread.h>
 
-#define FREEBOB_PLUGIN_VERSION "0.0.3"
+#define FREEBOB_PLUGIN_VERSION "0.0.7"
 
-#define PRINT_FUNCTION_ENTRY (printf("entering %s\n",__FUNCTION__))
+#define FREEBOB_USE_RT 			1
+#define FREEBOB_RT_PRIORITY_PACKETIZER 	60
+// midi priority should be higher than the audio priority in order to
+// make sure events are not only delivered on period boundarys
+// but I think it should be smaller than the packetizer thread in order not 
+// to lose any packets
+#define FREEBOB_RT_PRIORITY_MIDI 	59
 
+#define FREEBOB_WITH_MIDI
+
+#ifdef FREEBOB_WITH_MIDI
+
+	#define ALSA_SEQ_BUFF_SIZE 1024
+	#define MIDI_TRANSMIT_BUFFER_SIZE 1024
+	#define MIDI_THREAD_SLEEP_TIME_USECS 1000
+	
+	typedef struct {
+		int stream_nr;
+		int seq_port_nr;
+		snd_midi_event_t *parser;
+		snd_seq_t *seq_handle;
+	} freebob_midi_port_t;
+	
+	typedef struct _snd_pcm_freebob_midi_handle {
+		freebob_device_t *dev;
+		
+		snd_seq_t *seq_handle;
+		
+		pthread_t queue_thread;
+		pthread_t dequeue_thread;
+		int queue_thread_realtime;
+		int queue_thread_priority;
+	
+		int nb_input_ports;
+		int nb_output_ports;
+	
+		freebob_midi_port_t **input_ports;
+		freebob_midi_port_t **output_ports;
+	
+		freebob_midi_port_t **input_stream_port_map;
+		int *output_port_stream_map;
+	
+	
+	} snd_pcm_freebob_midi_handle_t;
+	
+
+#endif
+
+// #define PRINT_FUNCTION_ENTRY (printf("entering %s\n",__FUNCTION__))
+#define PRINT_FUNCTION_ENTRY
 typedef struct {
 	snd_pcm_ioplug_t io;
 
@@ -62,8 +110,18 @@ typedef struct {
 	// thread for polling
 	pthread_t thread;
 
+#ifdef FREEBOB_WITH_MIDI
+	snd_pcm_freebob_midi_handle_t *midi_handle;
+#endif
+
 } snd_pcm_freebob_t;
 
+#ifdef FREEBOB_WITH_MIDI
+	static snd_pcm_freebob_midi_handle_t *snd_pcm_freebob_midi_init(snd_pcm_freebob_t *driver);
+	static void snd_pcm_freebob_midi_finish (snd_pcm_freebob_midi_handle_t *m);
+#endif
+
+// unused
 static snd_pcm_sframes_t snd_pcm_freebob_write(snd_pcm_ioplug_t *io,
 				   const snd_pcm_channel_area_t *areas,
 				   snd_pcm_uframes_t offset,
@@ -117,7 +175,16 @@ static int snd_pcm_freebob_hw_params(snd_pcm_ioplug_t *io, snd_pcm_hw_params_t *
 
 		}
 	} else {
-		//TODO: implement capture
+		//fixme: assumes that the audio streams are always the first streams
+		for (i=0;i<freebob_streaming_get_nb_capture_streams(freebob->streaming_device);i++) {
+			if(freebob_streaming_get_capture_stream_type(freebob->streaming_device,i)==freebob_stream_type_audio) {
+				freebob->channels++;
+			}
+		}
+		for (i=0;i<freebob->channels;i++) {
+			// setup buffers, currently left at default
+
+		}
 	}
 
 	return 0;
@@ -127,7 +194,7 @@ static int
 snd_pcm_freebob_pollfunction(snd_pcm_freebob_t *freebob)
 {
 
- 	PRINT_FUNCTION_ENTRY;
+//  	PRINT_FUNCTION_ENTRY;
 	int retval;
 	static char buf[1];
 	snd_pcm_ioplug_t *io=&freebob->io;
@@ -153,10 +220,19 @@ snd_pcm_freebob_pollfunction(snd_pcm_freebob_t *freebob)
 	for (i=0;i<freebob->channels;i++) {
 		freebob_sample_t *buff=(freebob_sample_t *)((char *)areas[i].addr + (areas[i].step * offset / 8));
 // 		printf("%d: %p %d %d %p\n",i, areas[i].addr, offset, areas[i].step, buff);
-		freebob_streaming_set_playback_stream_buffer(freebob->streaming_device, i, (char *)buff, freebob_buffer_type_uint24);
+
+		if (freebob->stream == SND_PCM_STREAM_PLAYBACK) {
+			freebob_streaming_set_playback_stream_buffer(freebob->streaming_device, i, (char *)buff, freebob_buffer_type_uint24);
+		} else {
+			freebob_streaming_set_capture_stream_buffer(freebob->streaming_device, i, (char *)buff, freebob_buffer_type_uint24);
+		}
 	}
 
-	freebob_streaming_transfer_playback_buffers(freebob->streaming_device);
+	if (freebob->stream == SND_PCM_STREAM_PLAYBACK) {
+		freebob_streaming_transfer_playback_buffers(freebob->streaming_device);
+	} else {
+		freebob_streaming_transfer_capture_buffers(freebob->streaming_device);
+	}
 
 	freebob->hw_ptr += freebob->dev_options.period_size;
 	freebob->hw_ptr %= io->buffer_size;
@@ -187,6 +263,8 @@ static int snd_pcm_freebob_poll_revents(snd_pcm_ioplug_t *io,
 				     unsigned short *revents)
 {
 	static char buf[1];
+
+	PRINT_FUNCTION_ENTRY;
 	
 	assert(pfds && nfds == 1 && revents);
 
@@ -216,6 +294,13 @@ static int snd_pcm_freebob_close(snd_pcm_ioplug_t *io)
 {
 	PRINT_FUNCTION_ENTRY;
 	snd_pcm_freebob_t *freebob = io->private_data;
+
+	freebob_streaming_finish(freebob->streaming_device);
+
+#ifdef FREEBOB_WITH_MIDI
+	snd_pcm_freebob_midi_finish(freebob->midi_handle);	
+#endif	
+
 	snd_pcm_freebob_free(freebob);
 	return 0;
 }
@@ -226,7 +311,7 @@ static snd_pcm_sframes_t snd_pcm_freebob_pointer(snd_pcm_ioplug_t *io)
 
 	snd_pcm_freebob_t *freebob = io->private_data;
 
-	printf("%d\n", freebob->hw_ptr);
+// 	printf("%d\n", freebob->hw_ptr);
 	return freebob->hw_ptr;
 }
 
@@ -416,13 +501,13 @@ static int snd_pcm_freebob_open(snd_pcm_t **pcmp, const char *name,
 
 	freebob_device_info_t device_info;
 
-	freebob->dev_options.directions=0;
-
-	if(stream == SND_PCM_STREAM_PLAYBACK) {
-		freebob->dev_options.directions |= FREEBOB_IGNORE_CAPTURE;
-	} else {
-		freebob->dev_options.directions |= FREEBOB_IGNORE_PLAYBACK;
-	}
+ 	freebob->dev_options.directions=0;
+ 
+ 	if(stream == SND_PCM_STREAM_PLAYBACK) {
+ 		freebob->dev_options.directions |= FREEBOB_IGNORE_CAPTURE;
+ 	} else {
+ 		freebob->dev_options.directions |= FREEBOB_IGNORE_PLAYBACK;
+ 	}
 
 	freebob->streaming_device=freebob_streaming_init(&device_info,freebob->dev_options);
 
@@ -431,6 +516,15 @@ static int snd_pcm_freebob_open(snd_pcm_t **pcmp, const char *name,
 		freebob_destroy_handle( freebob->fb_handle );
 		return -EINVAL;
 	}
+
+#ifdef FREEBOB_WITH_MIDI
+	freebob->midi_handle=snd_pcm_freebob_midi_init(freebob);
+	if(!freebob->midi_handle) {
+		SNDERR("FREEBOB: Error creating midi device");
+		freebob_destroy_handle( freebob->fb_handle );
+		return -EINVAL;
+	}
+#endif
 
 /*
 	freebob->playback_channels = 0;
@@ -457,7 +551,7 @@ static int snd_pcm_freebob_open(snd_pcm_t **pcmp, const char *name,
 	freebob->io.callback = &freebob_pcm_callback;
 	freebob->io.private_data = freebob;
 	freebob->io.mmap_rw = 1;
-	freebob->io.poll_fd = freebob->fd;
+	freebob->io.poll_fd = fd[1];
 	freebob->io.poll_events = stream == SND_PCM_STREAM_PLAYBACK ? POLLOUT : POLLIN;
 
 	err = snd_pcm_ioplug_create(&freebob->io, name, stream, mode);
@@ -499,8 +593,8 @@ SND_PCM_PLUGIN_DEFINE_FUNC(freebob)
 
 	/* packetizer thread options */
 
-	dev_options.realtime=1;
-	dev_options.packetizer_priority=60;
+	dev_options.realtime=FREEBOB_USE_RT;
+	dev_options.packetizer_priority=FREEBOB_RT_PRIORITY_PACKETIZER;
 
 	snd_config_for_each(i, next, conf) {
 		snd_config_t *n = snd_config_iterator_entry(i);
@@ -562,3 +656,300 @@ SND_PCM_PLUGIN_DEFINE_FUNC(freebob)
 }
 
 SND_PCM_PLUGIN_SYMBOL(freebob);
+
+// midi stuff
+#ifdef FREEBOB_WITH_MIDI
+/*
+ * MIDI support
+ */ 
+
+// the thread that will queue the midi events from the seq to the stream buffers
+
+void * snd_pcm_freebob_midi_queue_thread(void *arg)
+{
+	snd_pcm_freebob_midi_handle_t *m=(snd_pcm_freebob_midi_handle_t *)arg;
+	assert(m);
+	snd_seq_event_t *ev;
+	unsigned char work_buffer[MIDI_TRANSMIT_BUFFER_SIZE];
+	int bytes_to_send;
+	int b;
+	int i;
+
+	SNDERR ("FREEBOB: MIDI queue thread started");
+
+	while(1) {
+		// get next event, if one is present
+		while ((snd_seq_event_input(m->seq_handle, &ev) > 0)) {
+			// get the port this event is originated from
+			freebob_midi_port_t *port=NULL;
+			for (i=0;i<m->nb_output_ports;i++) {
+				if(m->output_ports[i]->seq_port_nr == ev->dest.port) {
+					port=m->output_ports[i];
+					break;
+				}
+			}
+	
+			if(!port) {
+				SNDERR ("FREEBOB: Could not find target port for event: dst=%d src=%d\n", ev->dest.port, ev->source.port);
+
+				break;
+			}
+			
+			// decode it to the work buffer
+			if((bytes_to_send = snd_midi_event_decode ( port->parser, 
+				work_buffer,
+				MIDI_TRANSMIT_BUFFER_SIZE, 
+				ev))<0) 
+			{ // failed
+				SNDERR ("FREEBOB: Error decoding event for port %d (errcode=%d)\n", port->seq_port_nr,bytes_to_send);
+				bytes_to_send=0;
+				//return -1;
+			}
+	
+			for(b=0;b<bytes_to_send;b++) {
+				freebob_sample_t tmp_event=work_buffer[b];
+				if(freebob_streaming_write(m->dev, port->stream_nr, &tmp_event, 1)<1) {
+					SNDERR ("FREEBOB: Midi send buffer overrun\n");
+				}
+			}
+	
+		}
+
+		// sleep for some time
+		usleep(MIDI_THREAD_SLEEP_TIME_USECS);
+	}
+	return NULL;
+}
+
+// the dequeue thread (maybe we need one thread per stream)
+void *snd_pcm_freebob_midi_dequeue_thread (void *arg) {
+	snd_pcm_freebob_midi_handle_t *m=(snd_pcm_freebob_midi_handle_t *)arg;
+
+	int i;
+	int s;
+	
+	int samples_read;
+
+	assert(m);
+
+	while(1) {
+		// read incoming events
+	
+		for (i=0;i<m->nb_input_ports;i++) {
+			unsigned int buff[64];
+	
+			freebob_midi_port_t *port=m->input_ports[i];
+		
+			if(!port) {
+				SNDERR("FREEBOB: something went wrong when setting up the midi input port map (%d)",i);
+			}
+		
+			do {
+				samples_read=freebob_streaming_read(m->dev, port->stream_nr, buff, 64);
+			
+				for (s=0;s<samples_read;s++) {
+					unsigned int *byte=(buff+s) ;
+					snd_seq_event_t ev;
+					if ((snd_midi_event_encode_byte(port->parser,(*byte) & 0xFF, &ev)) > 0) {
+						// a midi message is complete, send it out to ALSA
+						snd_seq_ev_set_subs(&ev);  
+						snd_seq_ev_set_direct(&ev);
+						snd_seq_ev_set_source(&ev, port->seq_port_nr);
+						snd_seq_event_output_direct(port->seq_handle, &ev);						
+					}
+				}
+			} while (samples_read>0);
+		}
+
+		// sleep for some time
+		usleep(MIDI_THREAD_SLEEP_TIME_USECS);
+	}
+	return NULL;
+}
+
+static snd_pcm_freebob_midi_handle_t *snd_pcm_freebob_midi_init(snd_pcm_freebob_t *freebob) {
+// 	int err;
+
+	char buf[256];
+	int chn;
+	int nchannels;
+	int i=0;
+
+	freebob_device_t *dev=freebob->streaming_device;
+
+	assert(dev);
+
+	snd_pcm_freebob_midi_handle_t *m=calloc(1,sizeof(snd_pcm_freebob_midi_handle_t));
+	if (!m) {
+		SNDERR("FREEBOB: not enough memory to create midi structure");
+		return NULL;
+	}
+
+	if (snd_seq_open(&m->seq_handle, "default", SND_SEQ_OPEN_DUPLEX, SND_SEQ_NONBLOCK) < 0) {
+		SNDERR("FREEBOB: Error opening ALSA sequencer.");
+		free(m);
+		return NULL;
+	}
+
+	snd_seq_set_client_name(m->seq_handle, "FreeBob MIDI");
+
+	// find out the number of midi in/out ports we need to setup
+	nchannels=freebob_streaming_get_nb_capture_streams(dev);
+
+	m->nb_input_ports=0;
+
+	for (chn = 0; chn < nchannels; chn++) {	
+		if(freebob_streaming_get_capture_stream_type(dev, chn) == freebob_stream_type_midi) {
+			m->nb_input_ports++;
+		}
+	}
+
+	m->input_ports=calloc(m->nb_input_ports,sizeof(freebob_midi_port_t *));
+	if(!m->input_ports) {
+		SNDERR("FREEBOB: not enough memory to create midi structure");
+		free(m);
+		return NULL;
+	}
+
+	i=0;
+	for (chn = 0; chn < nchannels; chn++) {
+		if(freebob_streaming_get_capture_stream_type(dev, chn) == freebob_stream_type_midi) {
+			m->input_ports[i]=calloc(1,sizeof(freebob_midi_port_t));
+			if(!m->input_ports[i]) {
+				// fixme
+				SNDERR("FREEBOB: Could not allocate memory for seq port");
+				continue;
+			}
+
+	 		freebob_streaming_get_capture_stream_name(dev, chn, buf, sizeof(buf) - 1);
+			SNDERR("FREEBOB: Register MIDI IN port %s\n", buf);
+
+			m->input_ports[i]->seq_port_nr=snd_seq_create_simple_port(m->seq_handle, buf,
+				SND_SEQ_PORT_CAP_READ|SND_SEQ_PORT_CAP_SUBS_READ,
+				SND_SEQ_PORT_TYPE_MIDI_GENERIC);
+
+			if(m->input_ports[i]->seq_port_nr<0) {
+				SNDERR("FREEBOB: Could not create seq port");
+				m->input_ports[i]->stream_nr=-1;
+				m->input_ports[i]->seq_port_nr=-1;
+			} else {
+				m->input_ports[i]->stream_nr=chn;
+				m->input_ports[i]->seq_handle=m->seq_handle;
+				if (snd_midi_event_new  ( ALSA_SEQ_BUFF_SIZE, &(m->input_ports[i]->parser)) < 0) {
+					fprintf(stderr, "FREEBOB: could not init parser for MIDI IN port %d\n",i);
+					m->input_ports[i]->stream_nr=-1;
+					m->input_ports[i]->seq_port_nr=-1;
+				}
+			}
+
+			i++;
+		}
+	}
+
+	// playback
+	nchannels=freebob_streaming_get_nb_playback_streams(dev);
+
+	m->nb_output_ports=0;
+
+	for (chn = 0; chn < nchannels; chn++) {	
+		if(freebob_streaming_get_playback_stream_type(dev, chn) == freebob_stream_type_midi) {
+			m->nb_output_ports++;
+		}
+	}
+
+	m->output_ports=calloc(m->nb_output_ports,sizeof(freebob_midi_port_t *));
+	if(!m->output_ports) {
+		SNDERR("FREEBOB: not enough memory to create midi structure");
+		for (i = 0; i < m->nb_input_ports; i++) {	
+			free(m->input_ports[i]);
+		}
+		free(m->input_ports);
+		free(m);
+		return NULL;
+	}
+
+	i=0;
+	for (chn = 0; chn < nchannels; chn++) {
+		if(freebob_streaming_get_playback_stream_type(dev, chn) == freebob_stream_type_midi) {
+			m->output_ports[i]=calloc(1,sizeof(freebob_midi_port_t));
+			if(!m->output_ports[i]) {
+				// fixme
+				SNDERR("FREEBOB: Could not allocate memory for seq port");
+				continue;
+			}
+
+	 		freebob_streaming_get_playback_stream_name(dev, chn, buf, sizeof(buf) - 1);
+			SNDERR("FREEBOB: Register MIDI OUT port %s\n", buf);
+
+			m->output_ports[i]->seq_port_nr=snd_seq_create_simple_port(m->seq_handle, buf,
+				SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
+              			SND_SEQ_PORT_TYPE_MIDI_GENERIC);
+
+
+			if(m->output_ports[i]->seq_port_nr<0) {
+				SNDERR("FREEBOB: Could not create seq port");
+				m->output_ports[i]->stream_nr=-1;
+				m->output_ports[i]->seq_port_nr=-1;
+			} else {
+				m->output_ports[i]->stream_nr=chn;
+				m->output_ports[i]->seq_handle=m->seq_handle;
+				if (snd_midi_event_new  ( ALSA_SEQ_BUFF_SIZE, &(m->output_ports[i]->parser)) < 0) {
+					fprintf(stderr, "FREEBOB: could not init parser for MIDI OUT port %d\n",i);
+					m->output_ports[i]->stream_nr=-1;
+					m->output_ports[i]->seq_port_nr=-1;
+				}
+			}
+
+			i++;
+		}
+	}
+
+
+	// start threads
+	m->dev=dev;
+
+ 	m->queue_thread_priority=FREEBOB_RT_PRIORITY_MIDI;
+	m->queue_thread_realtime=FREEBOB_USE_RT;
+
+	if (pthread_create (&m->queue_thread, 0, snd_pcm_freebob_midi_queue_thread, (void *)m)) {
+		SNDERR("FREEBOB: cannot create midi queueing thread");
+		free(m);
+		return NULL;
+	}
+	
+	if (pthread_create (&m->dequeue_thread, 0, snd_pcm_freebob_midi_dequeue_thread, (void *)m)) {
+		SNDERR("FREEBOB: cannot create midi dequeueing thread");
+		free(m);
+		return NULL;
+	}
+	return m;
+}
+
+static void
+snd_pcm_freebob_midi_finish (snd_pcm_freebob_midi_handle_t *m)
+{
+	assert(m);
+
+	int i;
+
+	pthread_cancel (m->queue_thread);
+	pthread_join (m->queue_thread, NULL);
+
+	pthread_cancel (m->dequeue_thread);
+	pthread_join (m->dequeue_thread, NULL);
+
+
+	for (i=0;i<m->nb_input_ports;i++) {
+		free(m->input_ports[i]);
+
+	}
+	free(m->input_ports);
+
+	for (i=0;i<m->nb_output_ports;i++) {
+		free(m->output_ports[i]);
+	}
+	free(m->output_ports);
+
+	free(m);
+}
+#endif	
