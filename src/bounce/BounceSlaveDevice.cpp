31647d757814348d34655c7ea955e7e1a999b76d
pieterpalmers <pieterpalmers@2be59082-3212-0410-8809-b0798e1608f0>
2007-02-25 13:58:44 +0000

    - commit before rename
    git-svn-id: http://subversion.ffado.org/ffado/branches/streaming-rework@421 2be59082-3212-0410-8809-b0798e1608f0

---
+++ src/bounce/BounceSlaveDevice.cpp
@@ -0,0 +1,43 @@
+/* $Id$ */
+
+/*
+ *   FreeBob Streaming API
+ *   FreeBob = Firewire (pro-)audio for linux
+ *
+ *   http://freebob.sf.net
+ *
+ *   Copyright (C) 2007 Pieter Palmers <pieterpalmers@users.sourceforge.net>
+ *
+ *   This program is free software {} you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation {} either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY {} without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program {} if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * 
+ *
+ */
+
+#include "BounceSlaveDevice.h"
+
+namespace Bounce {
+
+IMPL_DEBUG_MODULE( BounceSlaveDevice, BounceSlaveDevice, DEBUG_LEVEL_NORMAL );
+
+BounceSlaveDevice::BounceSlaveDevice() {
+
+}
+
+BounceSlaveDevice::~BounceSlaveDevice() {
+
+}
+
+} // end of namespace Bounce
