3cb551528c6782f129a55120aa7fd068e7baaece
pieterpalmers <pieterpalmers@2be59082-3212-0410-8809-b0798e1608f0>
2007-03-11 13:15:41 +0000

    - dice support
    git-svn-id: http://subversion.ffado.org/ffado/branches/streaming-rework@433 2be59082-3212-0410-8809-b0798e1608f0

--- src/dice/dice_avdevice.h
+++ src/dice/dice_avdevice.h
@@ -24,14 +24,20 @@
 
 #include "debugmodule/debugmodule.h"
 #include "libavc/avc_definitions.h"
-#include "libfreebob/xmlparser.h"
 
 #include "libstreaming/AmdtpStreamProcessor.h"
+#include "libstreaming/AmdtpPort.h"
+#include "libieee1394/ARMHandler.h"
+
+#include <string>
+#include <vector>
 
 class ConfigRom;
 class Ieee1394Service;
 
 namespace Dice {
+
+class DiceNotifier;
 
 // struct to define the supported devices
 struct VendorModelEntry {
@@ -42,10 +48,12 @@ struct VendorModelEntry {
 };
 
 class DiceAvDevice : public IAvDevice {
+private:
+    class DiceNotifier;
 public:
     DiceAvDevice( std::auto_ptr<ConfigRom>( configRom ),
-	      Ieee1394Service& ieee1394Service,
-		  int nodeId);
+                  Ieee1394Service& ieee1394Service,
+                  int nodeId);
     ~DiceAvDevice();
 
     static bool probe( ConfigRom& configRom );
@@ -66,20 +74,116 @@ public:
     
     bool startStreamByIndex(int i);
     bool stopStreamByIndex(int i);
-
-    signed int getIsoRecvChannel(void);
-    signed int getIsoSendChannel(void);
   
 protected:
+    std::auto_ptr<ConfigRom>( m_configRom );
+    Ieee1394Service* m_p1394Service;
+    
     struct VendorModelEntry *m_model;
     
-    signed int m_iso_recv_channel, m_iso_send_channel;
+    // streaming stuff
+    typedef std::vector< Streaming::StreamProcessor * > StreamProcessorVector;
+    StreamProcessorVector m_receiveProcessors;
+    StreamProcessorVector m_transmitProcessors;
     
-	Streaming::AmdtpReceiveStreamProcessor *m_receiveProcessor;
-	Streaming::AmdtpTransmitStreamProcessor *m_transmitProcessor;
+private: // streaming & port helpers
+    enum EPortTypes {
+        ePT_Analog,
+        ePT_MIDI,
+    };
+    
+    typedef struct {
+        std::string name;
+        enum EPortTypes portType;
+        unsigned int streamPosition;
+        unsigned int streamLocation;
+    } diceChannelInfo; 
+    
+    bool addChannelToProcessor( diceChannelInfo *, 
+                              Streaming::StreamProcessor *, 
+                              Streaming::Port::E_Direction direction);
+    
+    int allocateIsoChannel(unsigned int packet_size);
+    bool deallocateIsoChannel(int channel);
+    
+private: // helper functions
+    bool enableIsoStreaming();
+    bool disableIsoStreaming();
+    bool isIsoStreamingEnabled();
+    
+    bool maskedCheckZeroGlobalReg(fb_nodeaddr_t offset, fb_quadlet_t mask);
+    bool maskedCheckNotZeroGlobalReg(fb_nodeaddr_t offset, fb_quadlet_t mask);
+    
+    typedef std::vector< std::string > diceNameVector;
+    diceNameVector splitNameString(std::string in);
+    diceNameVector getTxNameString(unsigned int i);
+    diceNameVector getRxNameString(unsigned int i);
+    diceNameVector getClockSourceNameString();
+    std::string getDeviceNickName();
+    
+private: // register I/O routines
+    bool initIoFunctions();
+    // quadlet read/write routines
+    bool readReg(fb_nodeaddr_t, fb_quadlet_t *);
+    bool writeReg(fb_nodeaddr_t, fb_quadlet_t);
+    bool readRegBlock(fb_nodeaddr_t, fb_quadlet_t *, size_t);
+    bool writeRegBlock(fb_nodeaddr_t, fb_quadlet_t *, size_t);
+    
+    bool readGlobalReg(fb_nodeaddr_t, fb_quadlet_t *);
+    bool writeGlobalReg(fb_nodeaddr_t, fb_quadlet_t);
+    bool readGlobalRegBlock(fb_nodeaddr_t, fb_quadlet_t *, size_t);
+    bool writeGlobalRegBlock(fb_nodeaddr_t, fb_quadlet_t *, size_t);
+    fb_nodeaddr_t globalOffsetGen(fb_nodeaddr_t, size_t);
+    
+    bool readTxReg(unsigned int i, fb_nodeaddr_t, fb_quadlet_t *);
+    bool writeTxReg(unsigned int i, fb_nodeaddr_t, fb_quadlet_t);
+    bool readTxRegBlock(unsigned int i, fb_nodeaddr_t offset, fb_quadlet_t *data, size_t length);
+    bool writeTxRegBlock(unsigned int i, fb_nodeaddr_t offset, fb_quadlet_t *data, size_t length);
+    fb_nodeaddr_t txOffsetGen(unsigned int, fb_nodeaddr_t, size_t);
+    
+    bool readRxReg(unsigned int i, fb_nodeaddr_t, fb_quadlet_t *);
+    bool writeRxReg(unsigned int i, fb_nodeaddr_t, fb_quadlet_t);
+    bool readRxRegBlock(unsigned int i, fb_nodeaddr_t offset, fb_quadlet_t *data, size_t length);
+    bool writeRxRegBlock(unsigned int i, fb_nodeaddr_t offset, fb_quadlet_t *data, size_t length);
+    fb_nodeaddr_t rxOffsetGen(unsigned int, fb_nodeaddr_t, size_t);
+    
+    fb_quadlet_t m_global_reg_offset;
+    fb_quadlet_t m_global_reg_size;
+    fb_quadlet_t m_tx_reg_offset;
+    fb_quadlet_t m_tx_reg_size;
+    fb_quadlet_t m_rx_reg_offset;
+    fb_quadlet_t m_rx_reg_size;
+    fb_quadlet_t m_unused1_reg_offset;
+    fb_quadlet_t m_unused1_reg_size;
+    fb_quadlet_t m_unused2_reg_offset;
+    fb_quadlet_t m_unused2_reg_size;
+    
+    fb_quadlet_t m_nb_tx;
+    fb_quadlet_t m_tx_size;
+    fb_quadlet_t m_nb_rx;
+    fb_quadlet_t m_rx_size;
+    
+private:
+    // notification
+    DiceNotifier *m_notifier;
+    
+    /**
+     * this class reacts on the DICE device writing to the 
+     * hosts notify address
+     */
+    #define DICE_NOTIFIER_BASE_ADDRESS 0x0000FFFFE0000000ULL
+    #define DICE_NOTIFIER_BLOCK_LENGTH 4
+    class DiceNotifier : public ARMHandler
+    {
+    public:
+        DiceNotifier(DiceAvDevice *, nodeaddr_t start);
+        virtual ~DiceNotifier();
+        
+    private:
+        DiceAvDevice *m_dicedevice;
+    };
 
 };
 
 }
-
 #endif
