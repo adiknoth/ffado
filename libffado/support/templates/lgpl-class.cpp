e38172ea393fc7f2cdac799cf541ab337bcffa24
ppalmers <ppalmers@2be59082-3212-0410-8809-b0798e1608f0>
2007-04-07 15:49:26 +0000

    - add some file templates and a script to instantiate them
    git-svn-id: http://subversion.ffado.org/ffado/trunk@446 2be59082-3212-0410-8809-b0798e1608f0

---
+++ libffado/support/templates/lgpl-class.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2007 by Daniel Wagner
+ * Copyright (C) 2007 by Pieter Palmers
+ * Copyright (C) 2007 by Jonathan Woithe
+ *
+ * This file is part of FFADO
+ * FFADO = Free Firewire (pro-)audio drivers for linux
+ *
+ * FFADO is based upon FreeBoB
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation;
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include "hhh"
+
+namespace YYY {
+
+IMPL_DEBUG_MODULE( XXX, XXX, DEBUG_LEVEL_NORMAL );
+
+XXX::XXX() {
+
+}
+
+XXX::~XXX() {
+
+}
+
+} // end of namespace YYY
