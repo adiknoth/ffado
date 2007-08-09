93e4bfe57d0437e3a51239e19526843d27e05ea5
jwoithe <jwoithe@2be59082-3212-0410-8809-b0798e1608f0>
2007-08-09 23:15:41 +0000

    Port remaining streaming tests to ffado and fix minor compilation errors.
    git-svn-id: http://subversion.ffado.org/ffado/trunk/libffado@514 2be59082-3212-0410-8809-b0798e1608f0

--- tests/streaming/testmidistreaming1.c
+++ tests/streaming/testmidistreaming1.c
@@ -44,7 +44,7 @@
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
 
-#include "libfreebob/freebob_streaming.h"
+#include "libffado/ffado.h"
 
 #include "debugtools.h"
 #define IEC61883_AM824_LABEL_MIDI_NO_DATA 0x80 
@@ -67,13 +67,13 @@ typedef struct {
 	int seq_port_nr;
 	snd_midi_event_t *parser;
 	snd_seq_t *seq_handle;
-} freebob_midi_port_t;
+} ffado_midi_port_t;
 
 typedef struct {
 	snd_seq_t *seq_handle;
 	int nb_seq_ports;
-	freebob_midi_port_t *ports[MAX_MIDI_PORTS];
-} freebob_midi_ports_t;
+	ffado_midi_port_t *ports[MAX_MIDI_PORTS];
+} ffado_midi_ports_t;
 
 int open_seq(snd_seq_t **seq_handle, int in_ports[], int out_ports[], int num_in, int num_out);
 
@@ -112,7 +112,7 @@ int open_seq(snd_seq_t **seq_handle, int in_ports[], int out_ports[], int num_in
 	return(0);
 }
 
-void decode_midi_byte (freebob_midi_port_t *port, int byte) {
+void decode_midi_byte (ffado_midi_port_t *port, int byte) {
 	snd_seq_event_t ev;
 	if ((snd_midi_event_encode_byte(port->parser,byte, &ev)) > 0) {
 // 		printf("message ok, sending it to %d\n", port->seq_port_nr);
@@ -126,7 +126,7 @@ void decode_midi_byte (freebob_midi_port_t *port, int byte) {
 	}
 }
 
-int encode_midi_bytes(freebob_midi_port_t *port, unsigned char *byte_buff, int len) {
+int encode_midi_bytes(ffado_midi_port_t *port, unsigned char *byte_buff, int len) {
 	return 0;
 }
 
@@ -139,23 +139,24 @@ int main(int argc, char *argv[])
 	int nb_in_channels=0, nb_out_channels=0;
 	int retval=0;
 	int i=0;
+	int start_flag = 0;
 
 	int nb_periods=0;
 
-	freebob_sample_t **audiobuffers_in;
-	freebob_sample_t **audiobuffers_out;
-	freebob_sample_t *nullbuffer;
+	ffado_sample_t **audiobuffers_in;
+	ffado_sample_t **audiobuffers_out;
+	ffado_sample_t *nullbuffer;
 	
 	run=1;
 
-	printf("Freebob MIDI streaming test application (1)\n");
+	printf("Ffado MIDI streaming test application (1)\n");
 
 	signal (SIGINT, sighandler);
 	signal (SIGPIPE, sighandler);
 
-	freebob_device_info_t device_info;
+	ffado_device_info_t device_info;
 
-	freebob_options_t dev_options;
+	ffado_options_t dev_options;
 
 	dev_options.sample_rate=-1; // -1 = detect from discovery
 	dev_options.period_size=PERIOD_SIZE;
@@ -168,56 +169,56 @@ int main(int argc, char *argv[])
 	dev_options.realtime=0;
 	dev_options.packetizer_priority=60;
 	
-	freebob_device_t *dev=freebob_streaming_init(&device_info, dev_options);
+	ffado_device_t *dev=ffado_streaming_init(&device_info, dev_options);
 	if (!dev) {
-		fprintf(stderr,"Could not init Freebob Streaming layer\n");
+		fprintf(stderr,"Could not init Ffado Streaming layer\n");
 		exit(-1);
 	}
 
-	nb_in_channels=freebob_streaming_get_nb_capture_streams(dev);
-	nb_out_channels=freebob_streaming_get_nb_playback_streams(dev);
+	nb_in_channels=ffado_streaming_get_nb_capture_streams(dev);
+	nb_out_channels=ffado_streaming_get_nb_playback_streams(dev);
 
 	int midi_in_nbchannels=0;
 	int midi_out_nbchannels=0;
 	
 	/* allocate intermediate buffers */
-	audiobuffers_in=calloc(nb_in_channels,sizeof(freebob_sample_t *));
-	audiobuffers_out=calloc(nb_in_channels,sizeof(freebob_sample_t));
+	audiobuffers_in=calloc(nb_in_channels,sizeof(ffado_sample_t *));
+	audiobuffers_out=calloc(nb_in_channels,sizeof(ffado_sample_t));
 	for (i=0;i<nb_in_channels;i++) {
-		audiobuffers_in[i]=calloc(PERIOD_SIZE+1,sizeof(freebob_sample_t));
-		audiobuffers_out[i]=calloc(PERIOD_SIZE+1,sizeof(freebob_sample_t));
+		audiobuffers_in[i]=calloc(PERIOD_SIZE+1,sizeof(ffado_sample_t));
+		audiobuffers_out[i]=calloc(PERIOD_SIZE+1,sizeof(ffado_sample_t));
 			
-		switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+		switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				/* assign the audiobuffer to the stream */
-				freebob_streaming_set_capture_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
-				freebob_streaming_set_capture_buffer_type(dev, i, freebob_buffer_type_float);
+				ffado_streaming_set_capture_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
+				ffado_streaming_set_capture_buffer_type(dev, i, ffado_buffer_type_float);
 				break;
 				
 			// this is done with read/write routines because the nb of bytes can differ.
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 				midi_in_nbchannels++;
 			default:
 				break;
 		}
 	}
 	
-	nullbuffer=calloc(PERIOD_SIZE+1,sizeof(freebob_sample_t));
+	nullbuffer=calloc(PERIOD_SIZE+1,sizeof(ffado_sample_t));
 	
 	for (i=0;i<nb_out_channels;i++) {
-		switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+		switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				if (i<nb_in_channels) {
 					/* assign the audiobuffer to the stream */
-					freebob_streaming_set_playback_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
-					freebob_streaming_set_playback_buffer_type(dev, i, freebob_buffer_type_float);
+					ffado_streaming_set_playback_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
+					ffado_streaming_set_playback_buffer_type(dev, i, ffado_buffer_type_float);
 				} else {
-					freebob_streaming_set_playback_stream_buffer(dev, i, (char *)nullbuffer);
-					freebob_streaming_set_playback_buffer_type(dev, i, freebob_buffer_type_int24);	
+					ffado_streaming_set_playback_stream_buffer(dev, i, (char *)nullbuffer);
+					ffado_streaming_set_playback_buffer_type(dev, i, ffado_buffer_type_int24);	
 				}
 				break;
 				// this is done with read/write routines because the nb of bytes can differ.
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 				midi_out_nbchannels++;
 			default:
 				break;
@@ -234,20 +235,20 @@ int main(int argc, char *argv[])
 
 		fid_out[i]=fopen(name,"w");
 
-		freebob_streaming_get_playback_stream_name(dev,i,name,sizeof(name));
+		ffado_streaming_get_playback_stream_name(dev,i,name,sizeof(name));
 		fprintf(fid_out[i],"Channel name: %s\n",name);
-		switch (freebob_streaming_get_playback_stream_type(dev,i)) {
-		case freebob_stream_type_audio:
+		switch (ffado_streaming_get_playback_stream_type(dev,i)) {
+		case ffado_stream_type_audio:
 			fprintf(fid_out[i],"Channel type: audio\n");
 			break;
-		case freebob_stream_type_midi:
+		case ffado_stream_type_midi:
 			fprintf(fid_out[i],"Channel type: midi\n");
 			break;
-		case freebob_stream_type_unknown:
+		case ffado_stream_type_unknown:
 			fprintf(fid_out[i],"Channel type: unknown\n");
 			break;
 		default:
-		case freebob_stream_type_invalid:
+		case ffado_stream_type_invalid:
 			fprintf(fid_out[i],"Channel type: invalid\n");
 			break;
 		}
@@ -257,20 +258,20 @@ int main(int argc, char *argv[])
 		snprintf(name,sizeof(name),"in_ch_%02d",i);
 		fid_in[i]=fopen(name,"w");
 
-		freebob_streaming_get_capture_stream_name(dev,i,name,sizeof(name));
-		fprintf(fid_in[i], "Channel name: %s\n");
-		switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-		case freebob_stream_type_audio:
+		ffado_streaming_get_capture_stream_name(dev,i,name,sizeof(name));
+		fprintf(fid_in[i], "Channel name: %s\n", name);
+		switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+		case ffado_stream_type_audio:
 			fprintf(fid_in[i], "Channel type: audio\n");
 			break;
-		case freebob_stream_type_midi:
+		case ffado_stream_type_midi:
 			fprintf(fid_in[i], "Channel type: midi\n");
 			break;
-		case freebob_stream_type_unknown:
+		case ffado_stream_type_unknown:
 			fprintf(fid_in[i],"Channel type: unknown\n");
 			break;
 		default:
-		case freebob_stream_type_invalid:
+		case ffado_stream_type_invalid:
 			fprintf(fid_in[i],"Channel type: invalid\n");
 			break;
 		}
@@ -287,19 +288,19 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	freebob_midi_port_t* midi_out_portmap[nb_out_channels];
-	freebob_midi_port_t* midi_in_portmap[nb_in_channels];
+	ffado_midi_port_t* midi_out_portmap[nb_out_channels];
+	ffado_midi_port_t* midi_in_portmap[nb_in_channels];
 	
 	int cnt=0;
 	
 	for (i=0;i<nb_out_channels;i++) {
-		freebob_midi_port_t *midi_out_port;
-		switch (freebob_streaming_get_playback_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+		ffado_midi_port_t *midi_out_port;
+		switch (ffado_streaming_get_playback_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				midi_out_portmap[i]=NULL;
 				break;
-			case freebob_stream_type_midi:
-				midi_out_port=malloc(sizeof(freebob_midi_port_t));
+			case ffado_stream_type_midi:
+				midi_out_port=malloc(sizeof(ffado_midi_port_t));
 				if(!midi_out_port) {
 					fprintf(stderr, "Could not allocate memory for MIDI OUT port %d\n",i);
 					exit(1); // this could be/is a memory leak, I know...
@@ -313,19 +314,20 @@ int main(int argc, char *argv[])
 					midi_out_portmap[i]=midi_out_port;
 				}
 				break;
+                        default: break;
 		}
 	}
 	
 	cnt=0;
 	for (i=0;i<nb_in_channels;i++) {
-		freebob_midi_port_t *midi_in_port;
-		switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+		ffado_midi_port_t *midi_in_port;
+		switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				midi_in_portmap[i]=NULL;
 				break;
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 		
-				midi_in_port=malloc(sizeof(freebob_midi_port_t));
+				midi_in_port=malloc(sizeof(ffado_midi_port_t));
 				if(!midi_in_port) {
 					fprintf(stderr, "Could not allocate memory for MIDI IN port %d\n",i);
 					exit(1); // this could be/is a memory leak, I know...
@@ -339,25 +341,26 @@ int main(int argc, char *argv[])
 					}   
 					midi_in_portmap[i]=midi_in_port;
 				}
-			break;
+				break;
+                        default: break;
 		}
 	}	
 	
 	// start the streaming layer
-	freebob_streaming_start(dev);
+	start_flag = ffado_streaming_start(dev);
 
 	fprintf(stderr,"Entering receive loop (%d,%d)\n",nb_in_channels,nb_out_channels);
-	while(run) {
-		retval = freebob_streaming_wait(dev);
+	while(run && start_flag==0) {
+		retval = ffado_streaming_wait(dev);
 		if (retval < 0) {
 			fprintf(stderr,"Xrun\n");
-			freebob_streaming_reset(dev);
+			ffado_streaming_reset(dev);
 			continue;
 		}
 		
-// 		freebob_streaming_transfer_buffers(dev);
-		freebob_streaming_transfer_capture_buffers(dev);
-		freebob_streaming_transfer_playback_buffers(dev);
+// 		ffado_streaming_transfer_buffers(dev);
+		ffado_streaming_transfer_capture_buffers(dev);
+		ffado_streaming_transfer_playback_buffers(dev);
 		
 		nb_periods++;
 
@@ -368,14 +371,14 @@ int main(int argc, char *argv[])
 		for(i=0;i<nb_in_channels;i++) {
 			int s;
 			
-			switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+			switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				// no need to get the buffers manually, we have set the API internal buffers to the audiobuffer[i]'s
-// 				//samplesread=freebob_streaming_read(dev, i, audiobuffer[i], PERIOD_SIZE);
+// 				//samplesread=ffado_streaming_read(dev, i, audiobuffer[i], PERIOD_SIZE);
 				samplesread=PERIOD_SIZE;
 				break;
-			case freebob_stream_type_midi:
-				samplesread=freebob_streaming_read(dev, i, audiobuffers_in[i], PERIOD_SIZE);
+			case ffado_stream_type_midi:
+				samplesread=ffado_streaming_read(dev, i, audiobuffers_in[i], PERIOD_SIZE);
 				quadlet_t *buff=(quadlet_t *)audiobuffers_in[i];
 				for (s=0;s<samplesread;s++) {
 					quadlet_t *byte=(buff+s) ;
@@ -384,17 +387,18 @@ int main(int argc, char *argv[])
 				}
 				if(samplesread>0) {
 	   				fprintf(fid_in[i], "---- Period read (%d samples) ----\n",samplesread);
-					hexDumpToFile(fid_in[i],(unsigned char*)audiobuffers_in[i],samplesread*sizeof(freebob_sample_t));
+					hexDumpToFile(fid_in[i],(unsigned char*)audiobuffers_in[i],samplesread*sizeof(ffado_sample_t));
 				}
 				break;
+                        default: break;
 			}
 //   			fprintf(fid_in[i], "---- Period read (%d samples) ----\n",samplesread);
-//   			hexDumpToFile(fid_in[i],(unsigned char*)buff,samplesread*sizeof(freebob_sample_t));
+//   			hexDumpToFile(fid_in[i],(unsigned char*)buff,samplesread*sizeof(ffado_sample_t));
 	
 		}
 
 		for(i=0;i<nb_out_channels;i++) {
-			freebob_sample_t *buff;
+			ffado_sample_t *buff;
 			int b=0;
 			unsigned char* byte_buff;
 			
@@ -404,12 +408,12 @@ int main(int argc, char *argv[])
 				buff=nullbuffer;
 			}
 			
-			switch (freebob_streaming_get_playback_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
-//  				sampleswritten=freebob_streaming_write(dev, i, buff, PERIOD_SIZE);
+			switch (ffado_streaming_get_playback_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
+//  				sampleswritten=ffado_streaming_write(dev, i, buff, PERIOD_SIZE);
 //				sampleswritten=PERIOD_SIZE;
 				break;
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 				
 				#define max_midi_bytes_to_write PERIOD_SIZE/8
 				byte_buff=(unsigned char*)buff;
@@ -417,14 +421,15 @@ int main(int argc, char *argv[])
 				sampleswritten=encode_midi_bytes(midi_out_portmap[i], byte_buff, max_midi_bytes_to_write);
 				
 				for(b=0;b<sampleswritten;b++) {
-					freebob_sample_t tmp_event=*(byte_buff+b);
-					freebob_streaming_write(dev, i, &tmp_event, 1);
+					ffado_sample_t tmp_event=*(byte_buff+b);
+					ffado_streaming_write(dev, i, &tmp_event, 1);
 				}
 				
 				
 				fprintf(fid_out[i], "---- Period write (%d samples) ----\n",sampleswritten);
-				hexDumpToFile(fid_out[i],(unsigned char*)buff,sampleswritten*sizeof(freebob_sample_t));
+				hexDumpToFile(fid_out[i],(unsigned char*)buff,sampleswritten*sizeof(ffado_sample_t));
 				break;
+                        default: break;
 			}
 		}
 
@@ -434,9 +439,9 @@ int main(int argc, char *argv[])
 
 	fprintf(stderr,"Exiting receive loop\n");
 	
-	freebob_streaming_stop(dev);
+	ffado_streaming_stop(dev);
 
-	freebob_streaming_finish(dev);
+	ffado_streaming_finish(dev);
 
 	for (i=0;i<nb_out_channels;i++) {
 		fclose(fid_out[i]);
@@ -456,7 +461,7 @@ int main(int argc, char *argv[])
 	
 	// free the MIDI to seq parsers and port structures
 	for(i=0;i<midi_in_nbchannels;i++) {
-		freebob_midi_port_t *midi_in_port=midi_in_portmap[i];
+		ffado_midi_port_t *midi_in_port=midi_in_portmap[i];
 		
 		if(midi_in_port) {
 			snd_midi_event_free  (  midi_in_port->parser );
@@ -465,7 +470,7 @@ int main(int argc, char *argv[])
 	}
 	// free the MIDI to seq parsers and port structures
 	for(i=0;i<midi_out_nbchannels;i++) {
-		freebob_midi_port_t *midi_out_port=midi_out_portmap[i];
+		ffado_midi_port_t *midi_out_port=midi_out_portmap[i];
 
 		if(midi_out_port) {
 			snd_midi_event_free  (  midi_out_port->parser );
--- tests/streaming/teststreaming3.c
+++ tests/streaming/teststreaming3.c
@@ -37,7 +37,7 @@
 
 #include <signal.h>
 
-#include "libfreebob/freebob_streaming.h"
+#include "libffado/ffado.h"
 
 #include "debugtools.h"
 
@@ -58,23 +58,24 @@ int main(int argc, char *argv[])
 	int nb_in_channels=0, nb_out_channels=0;
 	int retval=0;
 	int i=0;
+	int start_flag = 0;
 
 	int nb_periods=0;
 
 	float **audiobuffers_in;
-	freebob_sample_t **audiobuffers_out;
-	freebob_sample_t *nullbuffer;
+	ffado_sample_t **audiobuffers_out;
+	ffado_sample_t *nullbuffer;
 	
 	run=1;
 
-	printf("Freebob streaming test application (3)\n");
+	printf("Ffado streaming test application (3)\n");
 
 	signal (SIGINT, sighandler);
 	signal (SIGPIPE, sighandler);
 
-	freebob_device_info_t device_info;
+	ffado_device_info_t device_info;
 
-	freebob_options_t dev_options;
+	ffado_options_t dev_options;
 
 	dev_options.sample_rate=44100;
 	dev_options.period_size=PERIOD_SIZE;
@@ -87,51 +88,51 @@ int main(int argc, char *argv[])
 	dev_options.realtime=1;
 	dev_options.packetizer_priority=70;
 	
-	freebob_device_t *dev=freebob_streaming_init(&device_info, dev_options);
+	ffado_device_t *dev=ffado_streaming_init(&device_info, dev_options);
 	if (!dev) {
-		fprintf(stderr,"Could not init Freebob Streaming layer\n");
+		fprintf(stderr,"Could not init Ffado Streaming layer\n");
 		exit(-1);
 	}
 
-	nb_in_channels=freebob_streaming_get_nb_capture_streams(dev);
-	nb_out_channels=freebob_streaming_get_nb_playback_streams(dev);
+	nb_in_channels=ffado_streaming_get_nb_capture_streams(dev);
+	nb_out_channels=ffado_streaming_get_nb_playback_streams(dev);
 
 	/* allocate intermediate buffers */
 	audiobuffers_in=calloc(nb_in_channels,sizeof(float *));
-	audiobuffers_out=calloc(nb_in_channels,sizeof(freebob_sample_t));
+	audiobuffers_out=calloc(nb_in_channels,sizeof(ffado_sample_t));
 	for (i=0;i<nb_in_channels;i++) {
 		audiobuffers_in[i]=calloc(PERIOD_SIZE+1,sizeof(float));
-		audiobuffers_out[i]=calloc(PERIOD_SIZE+1,sizeof(freebob_sample_t));
+		audiobuffers_out[i]=calloc(PERIOD_SIZE+1,sizeof(ffado_sample_t));
 			
-		switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+		switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				/* assign the audiobuffer to the stream */
-				freebob_streaming_set_capture_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
-				freebob_streaming_set_capture_buffer_type(dev, i, freebob_buffer_type_float);
+				ffado_streaming_set_capture_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
+				ffado_streaming_set_capture_buffer_type(dev, i, ffado_buffer_type_float);
 				break;
 				// this is done with read/write routines because the nb of bytes can differ.
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 			default:
 				break;
 		}
 	}
 	
-	nullbuffer=calloc(PERIOD_SIZE+1,sizeof(freebob_sample_t));
+	nullbuffer=calloc(PERIOD_SIZE+1,sizeof(ffado_sample_t));
 	
 	for (i=0;i<nb_out_channels;i++) {
-		switch (freebob_streaming_get_playback_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+		switch (ffado_streaming_get_playback_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				if (i<nb_in_channels) {
 					/* assign the audiobuffer to the stream */
-					freebob_streaming_set_playback_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
-					freebob_streaming_set_playback_buffer_type(dev, i, freebob_buffer_type_float);
+					ffado_streaming_set_playback_stream_buffer(dev, i, (char *)(audiobuffers_in[i]));
+					ffado_streaming_set_playback_buffer_type(dev, i, ffado_buffer_type_float);
 				} else {
-					freebob_streaming_set_playback_stream_buffer(dev, i, (char *)nullbuffer);
-					freebob_streaming_set_playback_buffer_type(dev, i, freebob_buffer_type_int24);	
+					ffado_streaming_set_playback_stream_buffer(dev, i, (char *)nullbuffer);
+					ffado_streaming_set_playback_buffer_type(dev, i, ffado_buffer_type_int24);	
 				}
 				break;
 				// this is done with read/write routines because the nb of bytes can differ.
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 			default:
 				break;
 		}
@@ -147,20 +148,20 @@ int main(int argc, char *argv[])
 
 		fid_out[i]=fopen(name,"w");
 
-		freebob_streaming_get_playback_stream_name(dev,i,name,sizeof(name));
+		ffado_streaming_get_playback_stream_name(dev,i,name,sizeof(name));
 		fprintf(fid_out[i],"Channel name: %s\n",name);
-		switch (freebob_streaming_get_playback_stream_type(dev,i)) {
-		case freebob_stream_type_audio:
+		switch (ffado_streaming_get_playback_stream_type(dev,i)) {
+		case ffado_stream_type_audio:
 			fprintf(fid_out[i],"Channel type: audio\n");
 			break;
-		case freebob_stream_type_midi:
+		case ffado_stream_type_midi:
 			fprintf(fid_out[i],"Channel type: midi\n");
 			break;
-		case freebob_stream_type_unknown:
+		case ffado_stream_type_unknown:
 			fprintf(fid_out[i],"Channel type: unknown\n");
 			break;
 		default:
-		case freebob_stream_type_invalid:
+		case ffado_stream_type_invalid:
 			fprintf(fid_out[i],"Channel type: invalid\n");
 			break;
 		}
@@ -170,40 +171,41 @@ int main(int argc, char *argv[])
 		snprintf(name,sizeof(name),"in_ch_%02d",i);
 		fid_in[i]=fopen(name,"w");
 
-		freebob_streaming_get_capture_stream_name(dev,i,name,sizeof(name));
-		fprintf(fid_in[i], "Channel name: %s\n");
-		switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-		case freebob_stream_type_audio:
+		ffado_streaming_get_capture_stream_name(dev,i,name,sizeof(name));
+		fprintf(fid_in[i], "Channel name: %s\n",name);
+		switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+		case ffado_stream_type_audio:
 			fprintf(fid_in[i], "Channel type: audio\n");
 			break;
-		case freebob_stream_type_midi:
+		case ffado_stream_type_midi:
 			fprintf(fid_in[i], "Channel type: midi\n");
 			break;
-		case freebob_stream_type_unknown:
+		case ffado_stream_type_unknown:
 			fprintf(fid_in[i],"Channel type: unknown\n");
 			break;
 		default:
-		case freebob_stream_type_invalid:
+		case ffado_stream_type_invalid:
 			fprintf(fid_in[i],"Channel type: invalid\n");
 			break;
 		}
 	}
 
 	// start the streaming layer
-	freebob_streaming_start(dev);
+	ffado_streaming_prepare(dev);
+	start_flag = ffado_streaming_start(dev);
 
 	fprintf(stderr,"Entering receive loop (%d,%d)\n",nb_in_channels,nb_out_channels);
-	while(run) {
-		retval = freebob_streaming_wait(dev);
+	while(run && start_flag==0) {
+		retval = ffado_streaming_wait(dev);
 		if (retval < 0) {
 			fprintf(stderr,"Xrun\n");
-			freebob_streaming_reset(dev);
+			ffado_streaming_reset(dev);
 			continue;
 		}
 		
-// 		freebob_streaming_transfer_buffers(dev);
-		freebob_streaming_transfer_capture_buffers(dev);
-		freebob_streaming_transfer_playback_buffers(dev);
+// 		ffado_streaming_transfer_buffers(dev);
+		ffado_streaming_transfer_capture_buffers(dev);
+		ffado_streaming_transfer_playback_buffers(dev);
 		
 		nb_periods++;
 
@@ -214,15 +216,16 @@ int main(int argc, char *argv[])
 		for(i=0;i<nb_in_channels;i++) {
 			
 			
-			switch (freebob_streaming_get_capture_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+			switch (ffado_streaming_get_capture_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 				// no need to get the buffers manually, we have set the API internal buffers to the audiobuffer[i]'s
 // 				//samplesread=freebob_streaming_read(dev, i, audiobuffer[i], PERIOD_SIZE);
 				samplesread=PERIOD_SIZE;
 				break;
-			case freebob_stream_type_midi:
-				samplesread=freebob_streaming_read(dev, i, audiobuffers_out[i], PERIOD_SIZE);
+			case ffado_stream_type_midi:
+				samplesread=ffado_streaming_read(dev, i, audiobuffers_out[i], PERIOD_SIZE);
 				break;
+                        default: break;
 			}
 	
 //   			fprintf(fid_in[i], "---- Period read  (%d samples) ----\n",samplesread);
@@ -230,7 +233,7 @@ int main(int argc, char *argv[])
 		}
 
 		for(i=0;i<nb_out_channels;i++) {
-			freebob_sample_t *buff;
+			ffado_sample_t *buff;
 			int sampleswritten=0;
 			if (i<nb_in_channels) {
 				buff=audiobuffers_out[i];
@@ -238,17 +241,18 @@ int main(int argc, char *argv[])
 				buff=nullbuffer;
 			}
 			
-			switch (freebob_streaming_get_playback_stream_type(dev,i)) {
-			case freebob_stream_type_audio:
+			switch (ffado_streaming_get_playback_stream_type(dev,i)) {
+			case ffado_stream_type_audio:
 //  				sampleswritten=freebob_streaming_write(dev, i, buff, PERIOD_SIZE);
 				sampleswritten=PERIOD_SIZE;
 				break;
-			case freebob_stream_type_midi:
+			case ffado_stream_type_midi:
 // 				sampleswritten=freebob_streaming_write(dev, i, buff, PERIOD_SIZE);
 				break;
+                        default: break;
 			}
 //   			fprintf(fid_out[i], "---- Period write (%d samples) ----\n",sampleswritten);
-//   			hexDumpToFile(fid_out[i],(unsigned char*)buff,sampleswritten*sizeof(freebob_sample_t));
+//   			hexDumpToFile(fid_out[i],(unsigned char*)buff,sampleswritten*sizeof(ffado_sample_t));
 		}
 
 	}
@@ -257,9 +261,9 @@ int main(int argc, char *argv[])
 
 	fprintf(stderr,"Exiting receive loop\n");
 	
-	freebob_streaming_stop(dev);
+	ffado_streaming_stop(dev);
 
-	freebob_streaming_finish(dev);
+	ffado_streaming_finish(dev);
 
 	for (i=0;i<nb_out_channels;i++) {
 		fclose(fid_out[i]);
