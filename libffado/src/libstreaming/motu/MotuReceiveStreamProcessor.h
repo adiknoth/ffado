09045310fef5d4fac301e135a52ee1dc9639e340
jwoithe <jwoithe@2be59082-3212-0410-8809-b0798e1608f0>
2008-03-24 23:00:49 +0000

    config.h.in: reworked MOTU_* constants to get "nearly correct" timestamps in outgoing packets.  This makes playback at 2x and 4x samplerates work.  More fine-tuning may be needed.
    motu_avdevice.cpp: account for different port locations in packet data at 4x rates.
    
    motu_avdevice.cpp: MotuDevice::probe(), MotuDevice::createDevice(): remove commented out references to the ConfigRom ModelID field; with MOTUs this is useless for differentiating models.
    
    motu_avdevice.cpp: MotuDevice::setOpticalMode(): the 896HD doesn't have an SPDIF/TOSLINK optical mode, so don't try to set it.
    
    RME: implemented first cut at sample rate control.  This has about a 50% chance of working as it currently stands.
    
    MOTU: minor whitespace fixes for consistency.
    
    MOTU: start implementation of device status tracking.
    
    
    
    git-svn-id: http://subversion.ffado.org/ffado/trunk@981 2be59082-3212-0410-8809-b0798e1608f0

--- libffado/src/libstreaming/motu/MotuReceiveStreamProcessor.h
+++ libffado/src/libstreaming/motu/MotuReceiveStreamProcessor.h
@@ -36,6 +36,75 @@
 
 namespace Streaming {
 
+#define MOTUFW_MAX_MIXBUSES        4
+#define MOTUFW_MAX_MIXBUS_CHANNELS 20
+
+#define MOTU_CHANNEL_NORMAL        0x00
+#define MOTU_CHANNEL_MUTE          0x01
+#define MOTU_CHANNEL_SOLO          0x02
+#define MOTU_CHANNEL_PAIRED        0x08
+
+#define MOTU_CHANNEL_PAN_LEFT      0x00
+#define MOTU_CHANNEL_PAN_RIGHT     0x80
+#define MOTU_CHANNEL_PAN_CENTRE    0x40
+
+#define MOTU_DEST_DISABLED         0x00
+#define MOTU_DEST_HEADPHONE        0x01
+#define MOTU_DEST_ANALOG1_2        0x02
+#define MOTU_DEST_ANALOG3_4        0x03
+#define MOTU_DEST_ANALOG5_6        0x04
+#define MOTU_DEST_ANALOG7_8        0x05
+#define MOTU_DEST_AESEBU           0x06
+#define MOTU_DEST_SPDIF            0x07
+#define MOTU_DEST_ADAT1_1_2        0x08
+#define MOTU_DEST_ADAT1_3_4        0x09
+#define MOTU_DEST_ADAT1_5_6        0x0a
+#define MOTU_DEST_ADAT1_7_8        0x0b
+#define MOTU_DEST_MUTE             0x10
+
+enum EMotuDevCtrlStatus {
+  MOTU_DEVCTRL_INVALID           = 0x00,
+  MOTU_DEVCTRL_ACQUIRING         = 0x01,
+  MOTU_DEVCTRL_VALID             = 0x02,
+};
+
+struct MotuDevControls {
+    unsigned int status, lastkey;
+    unsigned int input_6dB_boost;
+    unsigned int input_ref_level;
+    unsigned int input_20dB_pad;
+    unsigned int input_gaintrim[MOTUFW_MAX_MIXBUS_CHANNELS];
+    unsigned char input_gaintrim_index;
+    struct MixBus {
+      unsigned char channel_gain[MOTUFW_MAX_MIXBUS_CHANNELS];
+      unsigned char channel_gain_index;
+      unsigned char channel_pan[MOTUFW_MAX_MIXBUS_CHANNELS];
+      unsigned char channel_pan_index;
+      unsigned char channel_control[MOTUFW_MAX_MIXBUS_CHANNELS];
+      unsigned char channel_control_index;
+      unsigned char bus_gain;
+      unsigned char bus_dest;
+    } mixbus[MOTUFW_MAX_MIXBUSES];
+    unsigned char mixbus_index;
+    unsigned char main_out_volume;
+    unsigned char phones_volume;
+    unsigned char phones_assign;
+};
+
+enum EMotuCtrlKeys {
+  MOTU_KEY_MIDI            = 0x01,
+  MOTU_KEY_CHANNEL_GAIN    = 0x14,
+  MOTU_KEY_CHANNEL_PAN     = 0x1c,
+  MOTU_KEY_CHANNEL_CTRL    = 0x24,
+  MOTU_KEY_MIXBUS_GAIN     = 0x2c,
+  MOTU_KEY_MIXBUS_DEST     = 0x34,
+  MOTU_KEY_MAINOUT_VOL     = 0x3c,
+  MOTU_KEY_PHONES_VOL      = 0x44,
+  MOTU_KEY_PHONES_DEST     = 0x4c,
+  MOTU_KEY_INPUT_6dB_BOOST = 0x6c,
+  MOTU_KEY_INPUT_REF_LEVEL = 0x74,
+};
+
 class MotuAudioPort;
 class MotuMidiPort;
 /*!
@@ -84,12 +153,16 @@ private:
 
     int decodeMotuEventsToPort(MotuAudioPort *, quadlet_t *data, unsigned int offset, unsigned int nevents);
     int decodeMotuMidiEventsToPort(MotuMidiPort *, quadlet_t *data, unsigned int offset, unsigned int nevents);
+    int decodeMotuCtrlEvents(char *data, unsigned int nevents);
 
     /*
      * An iso packet mostly consists of multiple events.  m_event_size
      * is the size of a single 'event' in bytes.
      */
     unsigned int m_event_size;
+
+    struct MotuDevControls m_devctrls;
+
 };
 
 
--- libffado/src/rme/rme_avdevice.cpp
+++ libffado/src/rme/rme_avdevice.cpp
@@ -55,6 +55,7 @@ RmeDevice::RmeDevice( DeviceManager& d,
     : FFADODevice( d, configRom )
     , m_model( NULL )
     , m_rme_model( RME_MODEL_NONE )
+    , m_ddsFreq( -1 )
 {
     debugOutput( DEBUG_LEVEL_VERBOSE, "Created Rme::RmeDevice (NodeID %d)\n",
                  getConfigRom().getNodeId() );
@@ -123,9 +124,17 @@ RmeDevice::discover()
 int
 RmeDevice::getSamplingFrequency( ) {
 /*
- * Retrieve the current sample rate from the RME device.
+ * Retrieve the current sample rate from the RME device.  At this stage it
+ * seems that the "current rate" can't be retrieved from the device.  Other
+ * drivers don't read the DDS control register and there isn't anywhere else
+ * where the frequency is sent back to the PC.  Unless we test the DDS
+ * control register for readabilty and find it can be read we'll assume it
+ * can't and instead cache the DDS frequency.
+ *
+ * If the device frequency has not been set this function will return -1
+ * (the default value of m_ddsFreq).
  */
-	return 48000;
+    return m_ddsFreq;
 }
 
 int
@@ -138,11 +147,32 @@ bool
 RmeDevice::setSamplingFrequency( int samplingFrequency )
 {
 /*
- * Set the RME device's samplerate.
+ * Set the RME device's samplerate.  The RME can do sampling frequencies of
+ * 32k, 44.1k and 48k along with the corresponding 2x and 4x rates. 
+ * However, it can also do +/- 4% from any of these "base" frequencies.
+ * This makes it a little long-winded to work out whether a given frequency
+ * is supported or not.
  */
-	if (samplingFrequency == 48000)
-		return true;
-	return false;
+
+    /* Work out whether the requested rate is supported */
+    if (!((samplingFrequency >= 32000*0.96 && samplingFrequency <= 32000*1.04) ||
+        (samplingFrequency >= 44100*0.96 && samplingFrequency <= 44100*1.04) ||
+        (samplingFrequency >= 48000*0.96 && samplingFrequency <= 48000*1.04) ||
+        (samplingFrequency >= 64000*0.96 && samplingFrequency <= 64000*1.04) ||
+        (samplingFrequency >= 88200*0.96 && samplingFrequency <= 88200*1.04) ||
+        (samplingFrequency >= 96000*0.96 && samplingFrequency <= 96000*1.04) ||
+        (samplingFrequency >= 128000*0.96 && samplingFrequency <= 128000*1.04) ||
+        (samplingFrequency >= 176000*0.96 && samplingFrequency <= 176000*1.04) ||
+        (samplingFrequency >= 192000*0.96 && samplingFrequency <= 192000*1.04))) {
+        return false;
+    }
+    
+    /* Send the desired frequency to the RME */
+    if (writeRegister(RME_REG_DDS_CONTROL, samplingFrequency) != 0)
+      return false;
+
+    m_ddsFreq = samplingFrequency;
+    return true;
 }
 
 FFADODevice::ClockSourceVector
@@ -212,4 +242,29 @@ RmeDevice::stopStreamByIndex(int i) {
 
 }
 
+unsigned int 
+RmeDevice::readRegister(unsigned int reg) {
+
+    quadlet_t quadlet;
+    
+    quadlet = 0;
+    if (get1394Service().read(0xffc0 | getNodeId(), reg, 1, &quadlet) < 0) {
+        debugError("Error doing RME read from register 0x%06x\n",reg);
+    }
+    return ntohl(quadlet);
+}
+
+signed int
+RmeDevice::writeRegister(unsigned int reg, quadlet_t data) {
+
+    unsigned int err = 0;
+    data = htonl(data);
+    if (get1394Service().write(0xffc0 | getNodeId(), reg, 1, &data) < 0) {
+        err = 1;
+        debugError("Error doing RME write to register 0x%06x\n",reg);
+    }
+//    SleepRelativeUsec(100);
+    return (err==0)?0:-1;
+}
+                  
 }
