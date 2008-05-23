5ac55d8a4a28858ff281a28f0f1d31a93a960fbc
wagi <wagi@2be59082-3212-0410-8809-b0798e1608f0>
2008-05-23 15:05:40 +0000

    fix DEBUG build
    git-svn-id: http://subversion.ffado.org/ffado/branches/libffado-2.0@1202 2be59082-3212-0410-8809-b0798e1608f0

--- src/libutil/PosixMutex.cpp
+++ src/libutil/PosixMutex.cpp
@@ -120,8 +120,7 @@ PosixMutex::Lock()
             debugError("Error locking the mutex: %d\n", err);
         }
     } else {
-        debugWarning("(%p) lock collision: %s got lock (from %s?)\n",
-                    this, name1, name2);
+        debugWarning("(%p) lock collision\n", this);
     }
     #else
     pthread_mutex_lock(&m_mutex);
