31c1f8f7792128ca4cccc6d01b505d1d5ec375bc
wagi <wagi@2be59082-3212-0410-8809-b0798e1608f0>
2008-09-20 10:40:09 +0000

    Add mixer control panel for FA 101. Also update the FA 66 to look and behave identical to the FA 101.
    git-svn-id: http://subversion.ffado.org/ffado/branches/libffado-2.0@1325 2be59082-3212-0410-8809-b0798e1608f0

---
+++ support/mixer/mixer_edirolfa101.py
@@ -0,0 +1,69 @@
+#
+# Copyright (C) 2005-2008 by Daniel Wagner
+#
+# This file is part of FFADO
+# FFADO = Free Firewire (pro-)audio drivers for linux
+#
+# FFADO is based upon FreeBoB.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from qt import *
+from mixer_edirolfa101ui import *
+
+class EdirolFa101Control(EdirolFa101ControlUI):
+    def __init__(self,parent = None,name = None,fl = 0):
+        EdirolFa101ControlUI.__init__(self,parent,name,fl)
+
+    def setComboMixSource(self,a0):
+            self.setSelector('line34source', a0)
+
+    def setVolumeIn12(self,a0):
+            self.setVolume('line12', a0)
+
+    def setVolumeIn34(self,a0):
+            self.setVolume('line34', a0)
+
+    def setVolumeIn56(self,a0):
+            self.setVolume('line56', a0)
+
+    def setVolumeIn78(self,a0):
+            self.setVolume('line78', a0)
+
+    def setVolumeIn910(self,a0):
+            self.setVolume('spdif', a0)
+
+    def setVolume(self,a0,a1):
+            name = a0
+            vol = -a1
+            print "setting %s volume to %d" % (name, vol)
+            self.hw.setContignuous(self.VolumeControls[name][0], vol)
+
+    def init(self):
+            print "Init Edirol FA-101 window"
+
+            self.VolumeControls={
+                'line12'  :   ['/Mixer/Feature_5', self.sldInput12],
+                'line34'  :   ['/Mixer/Feature_1', self.sldInput34],
+                'line56'  :   ['/Mixer/Feature_2', self.sldInput56],
+                'line78'  :   ['/Mixer/Feature_3', self.sldInput78],
+                'spdif'   :   ['/Mixer/Feature_4', self.sldInput910],
+                }
+
+    def initValues(self):
+            for name, ctrl in self.VolumeControls.iteritems():
+                vol = self.hw.getContignuous(ctrl[0])
+                print "%s volume is %d" % (name , vol)
+                ctrl[1].setValue(-vol)
