29b2dab76dbcbc1f201b208a360f4855014c5d26
arnonym <arnonym@2be59082-3212-0410-8809-b0798e1608f0>
2009-08-29 14:00:15 +0000

    More on the mixingmatrix. channels can be hidden now.
    git-svn-id: http://subversion.ffado.org/ffado/trunk@1639 2be59082-3212-0410-8809-b0798e1608f0

--- libffado/support/mixer-qt4/mixer_saffire_dice.py
+++ libffado/support/mixer-qt4/mixer_saffire_dice.py
@@ -22,65 +22,116 @@ from PyQt4 import QtGui, QtCore
 import dbus
 
 class MatrixNode(QtGui.QFrame):
-    def __init__(self,row,col,parent):
+    def __init__(self, row, col, parent):
         QtGui.QFrame.__init__(self,parent)
         self.row = row
+        self.connect(self.row, QtCore.SIGNAL("hide"), self.setHidden)
         self.column = col
+        self.connect(self.column, QtCore.SIGNAL("hide"), self.setHidden)
 
         self.setLineWidth(1)
         self.setFrameStyle(QtGui.QFrame.Panel|QtGui.QFrame.Raised)
         self.layout = QtGui.QGridLayout(self)
         self.setLayout(self.layout)
 
-        self.lbl = QtGui.QLabel("(%i,%i)" % (row,col), self)
-        self.layout.addWidget(self.lbl, 0, 0)
+        #self.lbl = QtGui.QLabel("(%i,%i)" % (row,col), self)
+        #self.layout.addWidget(self.lbl, 0, 0)
 
         self.dial = QtGui.QDial(self)
         self.dial.setRange(0,pow(2,16-1))
         self.connect(self.dial, QtCore.SIGNAL("valueChanged(int)"), self.valueChanged)
-        self.layout.addWidget(self.dial, 1, 0)
+        self.layout.addWidget(self.dial, 0, 0)
 
     def valueChanged(self, n):
-        self.emit(QtCore.SIGNAL("valueChanged"), self.row, self.column, n)
+        self.emit(QtCore.SIGNAL("valueChanged"), self.row.number, self.column.number, n)
+
+    def setHidden(self, hide):
+        if not hide:
+            if self.row.hidden or self.column.hidden:
+                return
+        QtGui.QFrame.setHidden(self, hide)
+
+class MatrixChannel(QtGui.QWidget):
+    def __init__(self, number, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        layout = QtGui.QGridLayout(self)
+        self.number = number
+        self.btn = QtGui.QPushButton("%i" % self.number, self)
+        self.btn.setCheckable(True)
+        self.connect(self.btn, QtCore.SIGNAL("clicked(bool)"), self.hideChannel)
+        layout.addWidget(self.btn, 0, 0)
+
+        self.hidden = False
+
+    def hideChannel(self, hide):
+        #print "MatrixChannel.hideChannel( %s )" % str(hide)
+        self.hidden = hide
+        self.emit(QtCore.SIGNAL("hide"), hide)
 
 class MatrixMixer(QtGui.QWidget):
-    def __init__(self,servername,basepath,parent=None):
+    def __init__(self, servername, basepath, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.bus = dbus.SessionBus()
         self.dev = self.bus.get_object(servername, basepath)
         self.interface = dbus.Interface(self.dev, dbus_interface="org.ffado.Control.Element.MatrixMixer")
 
-        rows = min(8, self.interface.getRowCount())
-        cols = min(8, self.interface.getColCount())
+        palette = self.palette()
+        print palette.color( QtGui.QPalette.Window ).name()
+        palette.setColor( QtGui.QPalette.Window, palette.color( QtGui.QPalette.Window ).darker() );
+        print self.palette().color( QtGui.QPalette.Window ).name()
+
+        rows = self.interface.getRowCount()
+        cols = self.interface.getColCount()
 
         layout = QtGui.QGridLayout(self)
         self.setLayout(layout)
 
+        self.columns = []
+        self.rows = []
+
+        # Add row/column headers
+        for i in range(cols):
+            ch = MatrixChannel(i, self)
+            layout.addWidget(ch, 0, i+1)
+            self.columns.append( ch )
+        for i in range(rows):
+            ch = MatrixChannel(i, self)
+            layout.addWidget(ch, i+1, 0)
+            self.rows.append( ch )
+
+        # Add node-widgets
         for i in range(rows):
             for j in range(cols):
-                node = MatrixNode(i, j, self)
+                node = MatrixNode(self.rows[i], self.columns[j], self)
                 node.dial.setValue(self.interface.getValue(i, j))
                 self.connect(node, QtCore.SIGNAL("valueChanged"), self.valueChanged)
-                layout.addWidget(node, i, j)
+                layout.addWidget(node, i+1, j+1)
 
     def valueChanged(self, row, column, n):
         #print "MatrixNode.valueChanged( %i, %i, %i )" % (row,column,n)
         self.interface.setValue(row, column, n)
+
+    def paintEvent(self, event):
+        p = QtGui.QPainter(self)
+        p.fillRect(event.rect(), self.palette().window())
 
 class Saffire_Dice(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.layout = QtGui.QGridLayout(self)
         self.setLayout(self.layout)
+        self.mixerwidget = QtGui.QScrollArea(self)
+        self.mixerwidget.setWidgetResizable(True)
+        self.layout.addWidget(self.mixerwidget)
 
     def buildMixer(self):
-        print self.hw
-        print self.hw.getText("/Generic/Nickname")
+        #print self.hw
+        #print self.hw.getText("/Generic/Nickname")
         self.matrix = MatrixMixer(self.hw.servername, self.hw.basepath+"/EAP/MatrixMixer", self)
-        self.layout.addWidget(self.matrix)
+        self.mixerwidget.setWidget(self.matrix)
 
-    def getDisplayTitle(self):
-        return "Experimental EAP Mixer"
+    #def getDisplayTitle(self):
+    #    return "Experimental EAP Mixer"
 
 
 #
