69433cd1aa9ee6d45a8317d89966c97e352d5dcd
jwoithe <jwoithe@2be59082-3212-0410-8809-b0798e1608f0>
2010-06-06 11:06:08 +0000

    MOTU: fix two typos in the mixer python code.  Thanks to Stefan Richter for spotting these.
    git-svn-id: http://subversion.ffado.org/ffado/branches/libffado-2.0@1846 2be59082-3212-0410-8809-b0798e1608f0

--- support/mixer-qt4/mixer_motu.py
+++ support/mixer-qt4/mixer_motu.py
@@ -706,7 +706,7 @@ class MotuMixer(QWidget, Ui_MotuMixerUI):
         # The Traveler has level and boost switchs for analog 5-8.  The 
         # Ultralite and 896HD don't implement them.  All other interfaces have
         # them over analog 1-8.
-        if (self.model==MOTU_MODEL_TRAVELER or self.model==MOTU_MODEL_ULTRALITE or self.model=MOTU_MODEL_896HD):
+        if (self.model==MOTU_MODEL_TRAVELER or self.model==MOTU_MODEL_ULTRALITE or self.model==MOTU_MODEL_896HD):
             self.disable_hide(self.ana1_level)
             self.disable_hide(self.ana2_level)
             self.disable_hide(self.ana3_level)
@@ -715,7 +715,7 @@ class MotuMixer(QWidget, Ui_MotuMixerUI):
             self.disable_hide(self.ana2_boost)
             self.disable_hide(self.ana3_boost)
             self.disable_hide(self.ana4_boost)
-        if (self.model==MOTU_MODEL_ULTRALITE or self.model=MOTU_MODEL_896HD):
+        if (self.model==MOTU_MODEL_ULTRALITE or self.model==MOTU_MODEL_896HD):
             self.disable_hide(self.ana5_level)
             self.disable_hide(self.ana6_level)
             self.disable_hide(self.ana7_level)
