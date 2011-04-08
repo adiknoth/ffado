23393d0710b0a7e62060fe66d552fa74223f6a59
adi <adi@2be59082-3212-0410-8809-b0798e1608f0>
2011-04-08 16:50:33 +0000

    More unlikely() in midi decoding.
    We don't expect any 2X/3X data to be present, hence it's utterly
    unlikely to hit this branch. Let's give a hint to the compiler so he can
    properly optimize.
    
    
    git-svn-id: http://subversion.ffado.org/ffado/trunk@1977 2be59082-3212-0410-8809-b0798e1608f0

--- libffado/src/libstreaming/amdtp/AmdtpReceiveStreamProcessor.cpp
+++ libffado/src/libstreaming/amdtp/AmdtpReceiveStreamProcessor.cpp
@@ -460,8 +460,8 @@ AmdtpReceiveStreamProcessor::decodeMidiPorts(quadlet_t *data,
 
                     debugOutputExtreme(DEBUG_LEVEL_VERBOSE, "(%p) MIDI [%d]: %08X\n", this,
                             i, sample_int);
-                } else if(IEC61883_AM824_HAS_LABEL(sample_int, IEC61883_AM824_LABEL_MIDI_2X)
-                        || IEC61883_AM824_HAS_LABEL(sample_int, IEC61883_AM824_LABEL_MIDI_3X) ) {
+                } else if(unlikely((IEC61883_AM824_HAS_LABEL(sample_int, IEC61883_AM824_LABEL_MIDI_2X)
+                        || IEC61883_AM824_HAS_LABEL(sample_int, IEC61883_AM824_LABEL_MIDI_3X) ))) {
                     debugOutput(DEBUG_LEVEL_VERBOSE, "Midi mode %X not supported.\n",
                             IEC61883_AM824_GET_LABEL(sample_int));
                 }
